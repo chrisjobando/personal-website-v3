bb357492a319f6cec635674a0d6fa6f38eabedfe
Chris <chrisjobando@gmail.com>
2020-03-31 12:51:03 -0400

    Made navbar larger

--- src/components/NavBar/NavMenu.jsx
+++ src/components/NavBar/NavMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Anime from 'react-anime';
 import { Link } from 'gatsby';
 
 // Styling
@@ -15,23 +16,33 @@ export default () => {
 
   return (
     <div className={style.NavMenu}>
-      <Link to="/" className={style.NavLink} activeClassName={style.ActiveNav}>
-        <h3>WORK.</h3>
-      </Link>
-      <Link
-        to="/about"
-        className={style.NavLink}
-        activeClassName={style.ActiveNav}
+      <Anime
+        opacity={[0, 1]}
+        translateY={['1em', 0]}
+        delay={(_, i) => i * 100 + 450}
       >
-        <h3>ABOUT.</h3>
-      </Link>
-      <Link
-        to="/resume"
-        className={style.NavLink}
-        activeClassName={style.ActiveNav}
-      >
-        <h3>RESUME.</h3>
-      </Link>
+        <Link
+          to="/"
+          className={style.NavLink}
+          activeClassName={style.ActiveNav}
+        >
+          <h3>WORK.</h3>
+        </Link>
+        <Link
+          to="/about"
+          className={style.NavLink}
+          activeClassName={style.ActiveNav}
+        >
+          <h3>ABOUT.</h3>
+        </Link>
+        <Link
+          to="/resume"
+          className={style.NavLink}
+          activeClassName={style.ActiveNav}
+        >
+          <h3>RESUME.</h3>
+        </Link>
+      </Anime>
     </div>
   );
 };
